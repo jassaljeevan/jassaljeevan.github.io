4ef7be9158bf164bc5528ee6d1620e4123d80ba9
jassaljeevan <jassaljeevan@gmail.com>
2015-12-29 00:31:11 +0000

    added 2 slider packages

--- packages.sh
+++ packages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/Fallout_4_Slider debs
 dpkg-deb -bZgzip projects/JassalJeevan_Icon_Pack debs
+dpkg-deb -bZgzip projects/The_Division_Slider debs
+dpkg-deb -bZgzip projects/Germany_4-Star_Slider debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
