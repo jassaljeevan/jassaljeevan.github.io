55dd564fd9168e8a5277d1742f83755efd9d0b9c
jassaljeevan <jassaljeevan@gmail.com>
2015-12-29 02:22:59 +0000

    added 2 packages

--- packages.sh
+++ packages.sh
@@ -3,4 +3,6 @@ dpkg-deb -bZgzip projects/Fallout_4_Slider debs
 dpkg-deb -bZgzip projects/JassalJeevan_Icon_Pack debs
 dpkg-deb -bZgzip projects/The_Division_Slider debs
 dpkg-deb -bZgzip projects/Germany_4-Star_Slider debs
+dpkg-deb -bZgzip projects/Assassins_Creed_Volume_HUD debs
+dpkg-deb -bZgzip projects/Danny_Phantom_Boot_Logo debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
